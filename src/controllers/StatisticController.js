6e4eb4abb9c994c6b34c4750b46eb43b94a9b2ad
buyan7 <Bkhishig1@gmail.com>
2023-09-27 18:15:17 +0800

    huviar zasvar

--- src/controllers/StatisticController.js
+++ src/controllers/StatisticController.js
@@ -17,13 +17,39 @@ module.exports = {
       D.DOCUMENT_NAME,
       D.DOCUMENT_SHORT_NAME,
       SU.USER_NAME,
-      SA.CREATED_DATE
+      SA.CREATED_DATE,
+      team.AUDITOR_NAME TEAM_MEMBER,
+      bat1.AUDITOR_NAME BAT1,
+      bat2.AUDITOR_NAME BAT2,
+      bat3.AUDITOR_NAME BAT3
       FROM AUD_STAT.STAT_AUDIT SA
       INNER JOIN AUD_STAT.REF_PERIOD RP ON SA.PERIOD_ID = RP.ID
       INNER JOIN AUD_ORG.REF_DEPARTMENT RD ON SA.DEPARTMENT_ID = RD.DEPARTMENT_ID
       INNER JOIN AUD_STAT.REF_DOCUMENT D ON SA.DOCUMENT_ID = D.ID
       LEFT JOIN AUD_REG.SYSTEM_USER SU ON SA.CREATED_BY = SU.USER_ID
-      WHERE SA.IS_ACTIVE = 1`;
+      LEFT JOIN ( SELECT PTD.STAT_AUDIT_ID,LISTAGG(SU.USER_NAME, ', ') WITHIN GROUP (ORDER BY ptd.ROLE_ID) AUDITOR_NAME
+  FROM AUD_STAT.STAT_AUDIT_TEAM ptd
+  INNER JOIN AUD_REG.SYSTEM_USER SU ON SU.USER_ID = ptd.AUDITOR_ID
+  WHERE ptd.IS_ACTIVE = 1 AND ptd.ROLE_ID = 1
+  GROUP BY PTD.STAT_AUDIT_ID) team ON team.STAT_AUDIT_ID = SA.ID 
+  
+  LEFT JOIN ( SELECT PTD.STAT_AUDIT_ID,LISTAGG(SU.USER_NAME, ', ') WITHIN GROUP (ORDER BY ptd.ROLE_ID) AUDITOR_NAME
+  FROM AUD_STAT.STAT_AUDIT_TEAM ptd
+  INNER JOIN AUD_REG.SYSTEM_USER SU ON SU.USER_ID = ptd.AUDITOR_ID
+  WHERE ptd.IS_ACTIVE = 1 AND ptd.ROLE_ID = 2
+  GROUP BY PTD.STAT_AUDIT_ID) bat1 ON bat1.STAT_AUDIT_ID = SA.ID 
+  
+   LEFT JOIN ( SELECT PTD.STAT_AUDIT_ID,LISTAGG(SU.USER_NAME, ', ') WITHIN GROUP (ORDER BY ptd.ROLE_ID) AUDITOR_NAME
+  FROM AUD_STAT.STAT_AUDIT_TEAM ptd
+  INNER JOIN AUD_REG.SYSTEM_USER SU ON SU.USER_ID = ptd.AUDITOR_ID
+  WHERE ptd.IS_ACTIVE = 1 AND ptd.ROLE_ID = 3
+  GROUP BY PTD.STAT_AUDIT_ID) bat2 ON bat2.STAT_AUDIT_ID = SA.ID 
+   LEFT JOIN ( SELECT PTD.STAT_AUDIT_ID,LISTAGG(SU.USER_NAME, ', ') WITHIN GROUP (ORDER BY ptd.ROLE_ID) AUDITOR_NAME
+  FROM AUD_STAT.STAT_AUDIT_TEAM ptd
+  INNER JOIN AUD_REG.SYSTEM_USER SU ON SU.USER_ID = ptd.AUDITOR_ID
+  WHERE ptd.IS_ACTIVE = 1 AND ptd.ROLE_ID = 4
+  GROUP BY PTD.STAT_AUDIT_ID) bat3 ON bat3.STAT_AUDIT_ID = SA.ID
+  WHERE SA.IS_ACTIVE = 1`;
 
       const binds = {};
       if (params.periodid) {
@@ -103,4 +129,42 @@ module.exports = {
       return errorFunction.saveErrorAndSend(req, res, err);
     }
   },
+  async getStatisticPlan(req,res){
+    try {
+      let params = {};
+      console.log(req.body);
+      if(req.body.STAT_ID === undefined || req.body.STAT_ID ===null)
+      return res.send("STAT_ID ilgeenuu");
+
+      params.STAT_ID = parseInt(req.body.STAT_ID, 10);
+      
+
+      let ListQuery = `SELECT ID, PERIOD_ID, DEPARTMENT_ID, DOCUMENT_ID, CONFIRM_DATE, STATUS, IS_ACTIVE, CREATED_BY, CREATED_DATE 
+      FROM AUD_STAT.STAT_AUDIT
+      WHERE ID = :STAT_ID`;
+
+      
+      const result = await OracleDB.simpleExecute(ListQuery, params);
+
+      let team_query = `SELECT ID,
+      STAT_AUDIT_ID,
+      AUDITOR_ID,
+      ROLE_ID,
+      sat.IS_ACTIVE,
+      sat.CREATED_BY ,
+      su.USER_CODE ,
+      su.USER_NAME 
+      FROM AUD_STAT.STAT_AUDIT_TEAM sat 
+      INNER JOIN AUD_REG."SYSTEM_USER" su ON su.USER_ID = sat.AUDITOR_ID 
+      WHERE STAT_AUDIT_ID = :STAT_ID`
+
+      const team_result = await OracleDB.simpleExecute(team_query, params);
+      if(result.rows !== undefined && result.rows.length>0)
+          return res.send( { Audit: result.rows[0], Team:team_result.rows,CREATED_BY:result.rows[0].CREATED_BY});
+
+
+    } catch (err) {
+      return errorFunction.saveErrorAndSend(req, res, err);
+    }
+  }
 };
--- src/routes/StatRoute.js
+++ src/routes/StatRoute.js
@@ -4,4 +4,5 @@ const StatisticController = require("../controllers/StatisticController");
 module.exports = (app) => {
   app.post("/statisticList", StatisticController.regStatisticList);
   app.post("/statisticIU", StatisticController.regStatisticIU);
+  app.post("/get_stat_plan", StatisticController.getStatisticPlan);
 };
