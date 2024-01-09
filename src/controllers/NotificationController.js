9424bf355ea0e0798c4a78d98e7124a42b634b54
baazka <baazka0705@gmail.com>
2024-01-09 15:32:00 +0800

    -note detail

--- src/controllers/NotificationController.js
+++ src/controllers/NotificationController.js
@@ -5,30 +5,35 @@ const { CheckNullInt, DateFormat } = require("../utils/Helper");
 module.exports = {
   async NotificationList(req, res) {
     try {
-      let ListQuery = `SELECT SN.ID, SN.AUDIT_ID, SN.DOCUMENT_ID, SN.CREATED_BY, SN.CREATED_DATE, SN.IS_SHOW, SN.REQUEST_TYPE, SN.DESCRIPTION, SN.PRO_STATUS,
-        RD.DOCUMENT_SHORT_NAME, RD.DOCUMENT_NAME, SU.USER_NAME, TD.AUDITOR_ID, 
-        SN.MODULE_ID
-        FROM AUD_REG.SYSTEM_NOTIFICATION SN
-        INNER JOIN (
-        SELECT SA.ID AUDIT_ID, 6 MODULE_ID, AUDITOR_ID, ROLE_ID 
-        FROM AUD_STAT.STAT_AUDIT SA 
-        INNER JOIN AUD_STAT.STAT_AUDIT_TEAM SAT ON SA.ID = SAT.STAT_AUDIT_ID 
-        WHERE SA.IS_ACTIVE = 1 AND SAT.IS_ACTIVE = 1 AND SAT.ROLE_ID IN (1,2,3,4)
-        ) TD ON SN.AUDIT_ID = TD.AUDIT_ID AND SN.MODULE_ID = TD.MODULE_ID AND TD.AUDITOR_ID = :USER_ID
-        LEFT JOIN AUD_STAT.REF_DOCUMENT RD ON SN.DOCUMENT_ID  = RD.ID
-        LEFT JOIN AUD_STAT.STAT_AUDIT SA ON SA.ID = SN.AUDIT_ID 
-        LEFT JOIN AUD_REG.SYSTEM_USER SU ON  SN.CREATED_BY = SU.USER_ID
-        WHERE SN.IS_ACTIVE  = 1 `;
-      let data = {}
-      if(CheckNullInt(req.body.AUDIT_ID) !== null){
-      ListQuery += ' AND SA.ID = :AUDIT_ID'
-      data = {AUDIT_ID:CheckNullInt(req.body.AUDIT_ID),USER_ID:CheckNullInt(req.body.USER_ID)}
-      }else{
-        data = {USER_ID:CheckNullInt(req.body.USER_ID)}
+      let ListQuery = `SELECT SN.ID, SN.AUDIT_ID, TD.DEPARTMENT_NAME, TD.PERIOD_LABEL, SN.DOCUMENT_ID, SN.CREATED_BY, SN.CREATED_DATE, SN.IS_SHOW, DECODE(SN.REQUEST_TYPE, 1, 'батлах', 2, 'цуцлах') REQUEST_TYPE, SN.DESCRIPTION, SN.PRO_STATUS,
+      RD.DOCUMENT_SHORT_NAME, RD.DOCUMENT_NAME, SU.USER_NAME, TD.AUDITOR_ID, TD.ROLE_ID,
+      SN.MODULE_ID
+      FROM AUD_REG.SYSTEM_NOTIFICATION SN
+      INNER JOIN (
+      SELECT SA.ID AUDIT_ID, RD.DEPARTMENT_NAME, RP.PERIOD_LABEL, 6 MODULE_ID, AUDITOR_ID, ROLE_ID 
+      FROM AUD_STAT.STAT_AUDIT SA 
+      INNER JOIN AUD_STAT.STAT_AUDIT_TEAM SAT ON SA.ID = SAT.STAT_AUDIT_ID 
+      INNER JOIN AUD_ORG.REF_DEPARTMENT RD ON SA.DEPARTMENT_ID = RD.DEPARTMENT_ID
+      INNER JOIN AUD_STAT.REF_PERIOD RP ON SA.PERIOD_ID = RP.ID
+      WHERE SA.IS_ACTIVE = 1 AND SAT.IS_ACTIVE = 1 AND SAT.ROLE_ID IN (1,2,3,4)
+      ) TD ON SN.AUDIT_ID = TD.AUDIT_ID AND SN.MODULE_ID = TD.MODULE_ID AND TD.AUDITOR_ID = :USER_ID
+      LEFT JOIN AUD_STAT.REF_DOCUMENT RD ON SN.DOCUMENT_ID  = RD.ID
+      LEFT JOIN AUD_STAT.STAT_AUDIT SA ON SA.ID = SN.AUDIT_ID 
+      LEFT JOIN AUD_REG.SYSTEM_USER SU ON  SN.CREATED_BY = SU.USER_ID
+      WHERE SN.IS_ACTIVE  = 1 `;
+      let data = {};
+      if (CheckNullInt(req.body.AUDIT_ID) !== null) {
+        ListQuery += "\n AND SA.ID = :AUDIT_ID";
+        data = {
+          AUDIT_ID: CheckNullInt(req.body.AUDIT_ID),
+          USER_ID: CheckNullInt(req.body.USER_ID),
+        };
+      } else {
+        data = { USER_ID: CheckNullInt(req.body.USER_ID) };
       }
       //ListQuery += `\nORDER BY C.CREATED_DATE DESC`;
-         console.log(data,'test');
-      const result = await OracleDB.simpleExecute(ListQuery,data);
+      console.log(data, "test");
+      const result = await OracleDB.simpleExecute(ListQuery, data);
       return res.send(result.rows);
       // if(result.rows !== undefined && result.rows.length>0)
       // return res.send(result.rows[0]);
@@ -51,7 +56,7 @@ module.exports = {
         P_DESCRIPTION: req.body.DESCRIPTION,
         P_CREATED_BY: req.body.CREATED_BY,
       };
-      console.log(data,'test');
+      console.log(data, "test");
 
       const result = await OracleDB.simpleExecute(queryInsert, data, {
         autoCommit: true,
