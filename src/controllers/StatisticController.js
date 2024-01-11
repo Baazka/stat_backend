d165b45f76e1464a99a9856414f3f5e0b77c2716
buyan7 <Bkhishig1@gmail.com>
2024-01-11 18:19:00 +0800

    test

--- src/controllers/StatisticController.js
+++ src/controllers/StatisticController.js
@@ -57,11 +57,11 @@ module.exports = {
       //     ListQuery += `\n AND EXISTS (SELECT AUDITOR_ID FROM AUD_STAT.STAT_AUDIT_TEAM T WHERE T.IS_ACTIVE = 1 AND T.AUDITOR_ID = :USER_ID AND T.STAT_AUDIT_ID = SA.ID)`;
       //   } //else binds = {};
       // }
-      if (params.periodid !== 999 && params.periodid !== null) {
+      if (params.periodid !== 999 && params.periodid !== null && params.periodid !== NaN) {
         ListQuery += `\n AND SA.PERIOD_ID = :PERIOD_ID`;
         binds.PERIOD_ID = CheckNullInt(params.periodid);
       }
-      if (params.depid !== 999) {
+      if (params.depid !== 999 && params.depid !== NaN) {
         ListQuery += `\n AND SA.DEPARTMENT_ID = :DEPARTMENT_ID`;
         binds.DEPARTMENT_ID = CheckNullInt(params.depid);
       }
